9bc9031b356f48f9cd5f451799b76f9b54383a5d
Mohammad Amin Khataei <makhataei@gmail.com>
2024-02-07 06:40:06 +0000

    Stg

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ from transformers import (
 import pandas as pd
 
 
-dd = pd.read_csv("./tot.csv")
+dd = pd.read_csv("tot.csv")
 
 class Settings(BaseSettings):
     """_summary_
@@ -27,14 +27,14 @@ class Settings(BaseSettings):
     OPENAPI_URL: str = ""
     ORIGINS: str = "*"
     ROOT_PATH: str = ""
-    SWAGGER_TITLE: str = "ChatBotBAK"
-    VERSION: str = "0.0.1"
+    SWAGGER_TITLE: str = "ChatBot"
+    VERSION: str = "1.0.0"
 
     APPLICATION_ID: str = "d7f48c21-2a19-4bdb-ace8-48928bff0eb5"
     # GRPC_IP: str = "172.24.65.20"
     # GRPC_PORT: int = 9035
-    SPLUNK_HOST: str = "172.24.65.206"
-    SPLUNK_PORT: int = 5141
+    SPLUNK_HOST: str = "127.0.0.1"
+    SPLUNK_PORT: int = 4521
     SPLUNK_INDEX: str = "dev"
 
     DATE_STRING: str = "%Y-%m-%d"
@@ -43,6 +43,8 @@ class Settings(BaseSettings):
     MODEL: str = "makhataei/qa-fa-mdeberta-v3-base"
     ABSTRACT: list = dd["question"].values.tolist()
     ANSWERS: list = dd["answer"].values.tolist()
+    LOG_LOCATION: str = "../test/logger.log"
+    JSON_LOCATION: str = "../test/logger.json"
 
 
 def loaders():
---
+++ src/tester.py
@@ -0,0 +1,53 @@
+import requests
+from src.config import settings
+from pymongo import MongoClient
+
+URL1 = "http://37.32.8.187:9001/response1"
+URL2 = "http://37.32.8.187:9001/response2"
+def get_database():
+    connection_sting ="mongodb://37.32.8.187:8081/"
+    client = MongoClient(connection_sting)
+    database = client["AnswerLogs"]
+
+    return database
+
+
+
+db = get_database()
+db.response1.delete_many({})
+db.response2.delete_many({})
+
+def requester(question):
+    total_request1 = requests.post(
+        URL1,
+        json={
+              "question": question,
+
+        },
+    )
+
+    response1 = total_request1.json()
+    try:
+        db.response1.insert_one(response1)
+    except:
+        pass
+
+    total_request2 = requests.post(
+        URL2,
+        json={
+              "question": question,
+
+        },
+    )
+
+    response2 = total_request2.json()
+    try:
+        db.response2.insert_one(response2)
+    except:
+        pass
+
+    # print(response)
+
+questions = []
+for q in questions:
+    requester(q)
