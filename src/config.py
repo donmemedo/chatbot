6fc84ee3580219bf8aff3a597d5c433368ee3557
Mohammad Amin Khataei <makhataei@gmail.com>
2024-04-14 11:33:47 +0000

    Stg

--- src/config.py
+++ src/config.py
@@ -3,16 +3,16 @@
 Returns:
     _type_: _description_
 """
+import pandas as pd
 from pydantic_settings import BaseSettings
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer
 from transformers import (
     AutoTokenizer,
     AutoModel,
 )
-import pandas as pd
 
+questions_answers = pd.read_csv("QAs.csv")
 
-dd = pd.read_csv("tot.csv")
 
 class Settings(BaseSettings):
     """_summary_
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
     ORIGINS: str = "*"
     ROOT_PATH: str = ""
     SWAGGER_TITLE: str = "ChatBot"
-    VERSION: str = "1.0.0"
+    VERSION: str = "1.1.0"
 
     APPLICATION_ID: str = "d7f48c21-2a19-4bdb-ace8-48928bff0eb5"
     # GRPC_IP: str = "172.24.65.20"
@@ -41,10 +41,11 @@ class Settings(BaseSettings):
     FASTAPI_DOCS: str = "/docs"
     FASTAPI_REDOC: str = "/redoc"
     MODEL: str = "makhataei/qa-fa-mdeberta-v3-base"
-    ABSTRACT: list = dd["question"].values.tolist()
-    ANSWERS: list = dd["answer"].values.tolist()
+    ABSTRACT: list = questions_answers["question"].values.tolist()
+    ANSWERS: list = questions_answers["answer"].values.tolist()
     LOG_LOCATION: str = "../test/logger.log"
     JSON_LOCATION: str = "../test/logger.json"
+    SCORE: int = 0.66
 
 
 def loaders():
@@ -55,15 +56,18 @@ def loaders():
         AutoTokenizer.from_pretrained(model_name_or_path)
         AutoModel.from_pretrained(model_name_or_path)
 
-    model1_cpu = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2", device="cpu")
-    model2_cpu = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", device="cpu")
+    model1_cpu = SentenceTransformer(
+        "sentence-transformers/paraphrase-multilingual-mpnet-base-v2", device="cpu"
+    )
+    model2_cpu = SentenceTransformer(
+        "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", device="cpu"
+    )
 
     sentences = settings.ABSTRACT
 
     embeddings1_cpu = model1_cpu.encode(sentences)
     embeddings2_cpu = model2_cpu.encode(sentences)
-    return model1_cpu,embeddings1_cpu,model2_cpu,embeddings2_cpu
-
+    return model1_cpu, embeddings1_cpu, model2_cpu, embeddings2_cpu
 
 
 settings = Settings()
